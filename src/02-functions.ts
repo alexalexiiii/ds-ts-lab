7d89337186390584eb87020ba3904ea93c828fa2
20101924ale <114234258+20101924ale@users.noreply.github.com>
2025-09-19 12:18:23 +0100

    Type aliases demonstration.

---
+++ src/02-functions.ts
@@ -0,0 +1,18 @@
+import {Friend, Colleague } from './myTypes'
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number,
+  max? : number
+): EmailContact[] {
+  let end = colleagues.length;
+  if (max !== undefined) {
+     end = max < 2 ? 1 : max
+  }
+  const sorted = colleagues.sort(sorter);
+  const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return fullResult.slice(0,end)
+}
+// Test invocations
+console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
---
+++ src/03-typeAliases.ts
@@ -0,0 +1,86 @@
+const colleague1: ColleagueV2 = {
+  name: "Ralph Graham",
+  department: "Engineering",
+  contact: {
+    email: "rgraham@company.com",
+    extension: 121,
+  },
+};
+
+
+
+export type Department = "Engineering" | "Finance" | "HR";
+export interface ColleagueV2 {
+  name: string;
+  department: Department;    // *****
+  contact: {
+    email: string;
+    extension: number;
+    slack?: string;
+  };
+}
+
+export type Buddy = Friend | ColleagueV2;
+export type Administrator = Buddy | string | undefined
+
+export type BuddyList = {
+  name: string;
+  administrator: Administrator;
+  members: Buddy[];
+};
+\function makeBuddyList(
+  name: string,
+  buddies: Buddy[],
+  admin?: Administrator
+): BuddyList {
+  return {
+    name,
+    members: buddies,
+    administrator: admin,
+  } as BuddyList;
+  // The as operator above casts an object to a specific type.
+}
+// Tests for makeBuddyList
+const myFootballBuddies = makeBuddyList(
+  "Football team",
+  [colleague1, friends[0], colleague2],
+  colleague1
+)
+
+const myBandBuddies = makeBuddyList(
+    "Band name",
+    [colleague1, friends[1]]
+    // No administrator
+  )
+
+console.log(myFootballBuddies)
+console.log(myBandBuddies)
+//--------------------------------------
+function findBuddyContact(list: BuddyList, name: string): string | undefined {
+  for (const buddy of list.members) {
+    if (buddy.name === name) {
+      if ("phone" in buddy) {
+        return buddy.phone;
+      }
+      else {
+        return buddy.contact.email;
+      }
+    }
+    return undefined;
+  }
+}
+
+function getBuddyListFriends(list: BuddyList): Friend[] {
+
+for (const buddy of list.members) {
+    if (buddy.name === list) {
+        if ("friend" in buddy){
+            return buddy.getBuddyListFriends
+        }
+        else {
+            return buddy;
+        }
+    }
+}
+// Test for findBuddyContact.
+console.log("Contact buddy at: ", findBuddyContact(myFootballBuddies, "Ralph Graham"));
