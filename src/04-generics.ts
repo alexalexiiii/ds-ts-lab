00f94494bfbefb0b3089e270ebb1c1a4c4b7d4d2
20101924ale <114234258+20101924ale@users.noreply.github.com>
2025-09-19 12:20:37 +0100

    Define types using Utility types.

---
+++ src/04-generics.ts
@@ -0,0 +1,9 @@
+import {friends, colleagues} from './01-basics'
+import {Friend, Colleague} from './myTypes'
+
+function findMatch<T>( data : T[], criteria: (d: T) => boolean ) : T | undefined {
+    return data.find((criteria))
+}
+
+console.log(findMatch<Friend>(friends, (f) => f.name.startsWith('Jane')  ))
+console.log(findMatch<Colleague>(colleagues.current, (c) => c.department === 'Finance'  ))
