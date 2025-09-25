46b94a55ced7de077976e4b324bf0902afdf6430
Alexandra Dinea <114234258+alexalexiiii@users.noreply.github.com>
2025-09-25 06:51:08 -0700

    event pass generation function, get collegue name and department while generating code

--- src/05-utitiltyTypes.ts
+++ src/05-utitiltyTypes.ts
@@ -10,3 +10,24 @@ console.log("Updated friend:", updateFriend(friends[0], {
     phone: '08712345',
     dob: new Date("1998-10-22")
 }));
+
+interface EventPass {
+    name: string;
+    department: string;
+    passCode: number;
+}
+
+// event pass function, generate pass code 
+function generateEventPass(colleague: Colleague): EventPass {
+    const passCode = Math.floor(Math.random() * 1000) + 1;
+    return {
+        name: colleague.name,
+        department: colleague.department,
+        passCode
+    };
+}
+
+console.log("Event pass:", generateEventPass(colleagues.current[0]));
+
+
+
