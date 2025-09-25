654b915efd318857721d8fec44faa807635733ff
Alexandra Dinea <114234258+alexalexiiii@users.noreply.github.com>
2025-09-25 06:49:17 -0700

    added update frined function to utility file types

--- src/05-utitiltyTypes.ts
+++ src/05-utitiltyTypes.ts
@@ -1,52 +1,12 @@
 import { friends, colleagues } from "./01-basics";
-import { Friend, Colleague, SecureFriendContact, FriendPartial } from "./myTypes";
+import { Friend, Colleague, FriendPartial } from "./myTypes";
 
-function updateFriend(friend: Friend, updates: FriendPartial ) : Friend {
-  return { ...friend, ...updates}
+// update friend function, used to update existing friend  
+function updateFriend(friend: Friend, updates: FriendPartial): Friend {
+    return { ...friend, ...updates };
 }
 
-function generateEventPass(colleague: Colleague): EventPass {
-  const passCode = Math.round(Math.random() * (1000 - 1) + 1);
-  return {
-    name: colleague.name,
-    department: colleague.department,
-    passCode: passCode,
-  };
-}
-console.log(generateEventPass(colleagues.current[0]));
-function intersection(
-  friends: Friend[],
-  colleagues: Colleague[]
-): Array<Friend & Colleague> {  // Intersection type
-  return friends.reduce((res: Array<Friend & Colleague>, friend) => {
-    const colleague = colleagues.find((col) => col.name === friend.name);
-    if (colleague) {
-      res.push({ ...friend, ...colleague });
-    }
-    return res;
-  }, []);
-}
-
-console.log(intersection(friends, colleagues.current));
-
-function sortFriend<>
-
-
-
-
-console.log(updateFriend(friends[0], {
-  phone: '08712345',
-  dob: new Date("1998-10-22")
-}))
-
-import { friends, colleagues } from "./01-basics";
-import { Friend, Colleague, SecureFriendContact, FriendPartial } from "./myTypes";
-
-function updateFriend(friend: Friend, updates: FriendPartial ) : Friend {
-  return { ...friend, ...updates}
-}
-
-console.log(updateFriend(friends[0], {
-  phone: '08712345',
-  dob: new Date("1998-10-22")
-}))
+console.log("Updated friend:", updateFriend(friends[0], {
+    phone: '08712345',
+    dob: new Date("1998-10-22")
+}));
